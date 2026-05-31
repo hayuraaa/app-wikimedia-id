0951a98f6592350397bf6a2d50e3e4912fab97a6
Hayuraaa <yunandaharis@gmail.com>
2026-05-31 08:27:40 +0700

    redirect

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/sumbangan",
+        destination: "/donasi",
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "upload.wikimedia.org" },
